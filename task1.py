90876b6c61a56034c5fa7b2d52380e9378fc5245
sughnenweb3 <jforster460@gmail.com>
2025-08-01 14:12:37 +0100

    task 1

--- task1.py
+++ task1.py
@@ -9,3 +9,13 @@ Later in the day:
 - The "Student" category sold out completely.
 - The team wanted to keep a record of the day’s sales before preparing for next week’s concert.
 """
+record = []
+tickets = {"VIP": 50, "Regular": 150, "Student": 75}
+new_ticket = {"backstage":10}
+tickets.update(new_ticket)
+print("Tickets before popping:", tickets)
+tickets.pop("Student")
+print("When students category sold out: ", tickets)
+record.append(tickets)
+print(record)
+print(tickets)
